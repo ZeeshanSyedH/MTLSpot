f2be0c94cbb6d6a4d0bea5895837166a8c662846
Zeeshan Syed <zeeshan@securite06.montreal.lasalle-intl.com>
2017-08-07 14:06:44 -0400

    Added data templates in view

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -1,7 +1,40 @@
 /* GET HOME PAGE */
 module.exports.homelist=function(req,res)
 {
-    res.render('locations-list',{title:'Home'});
+    res.render('locations-list',
+    {
+        title:'MTLSpot - Find a place to work with wifi',
+        pageHeader: 
+        {
+            title:'MTLSpot',
+            strapline: 'Find places in Montreal to eat, meet or just hang out !'
+        },
+        sidebar: 'Looking for wifi and a seat? MTLSpot helps you find places to work when out and about. Perhaps with coffee, cake or a pint? Let MTLSpot help you find the place you are looking for.',
+        locations:
+        [
+            {
+                name:'Starcups',
+                address: '125 High Street, Reading, RG6 1PS',
+                rating:3,
+                facilities:['Hot drinks', 'Food', 'Premium Wifi'],
+                distance:'100m'
+            },
+            {
+                name:'Cafe Hero',
+                address: '125 High Street, Reading, RG6 1PS',
+                rating:5,
+                facilities:['Hot drinks', 'Food', 'Premium Wifi'],
+                distance:'100m'
+            },
+            {
+                name:'Burger Queen',
+                address: '125 High Street, Reading, RG6 1PS',
+                rating:2,
+                facilities:['Hot drinks', 'Food', 'Premium Wifi'],
+                distance:'250'
+            }
+        ]
+    });
 };
 
 module.exports.locationInfo=function(req,res)
